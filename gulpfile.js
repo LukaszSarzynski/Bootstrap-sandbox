8ee04cc5fc6f53a39013b7a9f447134b18c6c8e8
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-07-27 20:24:30 +0200

    add gulp task to compile bootstrap sass source

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-function hello(cb) {
-  console.log('hello from Gulp');
-  cb();
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass');
+
+function compileBootstrap() {
+  return src('src/02_gulp_template/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('src/02_gulp_template/css'));
+    
 }
 
-exports.default = hello;
+exports.default = compileBootstrap;
