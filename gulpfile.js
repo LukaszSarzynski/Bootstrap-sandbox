962eff3a6ad00540da8eb4096de53cb73319b156
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2019-07-28 12:08:07 +0200

    add BrowserSync and file change watch on html/scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,43 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch,   series } = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+
+const watchHtmlGulpTemplate = watch(['src/02_gulp_template/*.html']);
+const watchScssGulpTemplate = watch(['src/02_gulp_template/scss/*.scss']);
+
+watchScssGulpTemplate.on('change', function(path, stats) {
+  console.log(`SASS File ${path} was changed`);
+  compileBootstrap();
+  reloadBrowserSync();
+});
+watchHtmlGulpTemplate.on('change', function(path, stats) {
+  console.log(`HTML File ${path} was changed`);
+  reloadBrowserSync();
+});
+
+function reloadBrowserSync() {
+  console.log(` >reload browseSync..`);
+  browserSync.reload();
+}
 
 function compileBootstrap() {
+  console.log(' >complie sass bootrsrap to `src/02_gulp_template/css` ')  
   return src('src/02_gulp_template/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('src/02_gulp_template/css'));
     
 }
 
-exports.default = compileBootstrap;
+function rserve(cb) {
+
+  console.log(' >run browseSync ')
+  browserSync({
+    server: 'src/02_gulp_template'
+  });
+
+  return cb();
+};
+
+exports.rserve = rserve;
+
+exports.default = series(compileBootstrap,rserve); 
